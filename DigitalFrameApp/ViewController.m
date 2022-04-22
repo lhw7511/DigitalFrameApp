eab80bbbf748468b3ad8b06658927aec4e78368c
hyunwooklee <lhw751@naver.com>
2022-04-22 10:31:51 +0900

    image animating

--- DigitalFrameApp/ViewController.m
+++ DigitalFrameApp/ViewController.m
@@ -12,10 +12,41 @@
 @end
 
 @implementation ViewController
+-(IBAction)toggleAction:(id)sender{
+    if([imgView isAnimating]){
+        [imgView stopAnimating];
+        [toggleButton setTitle:@"START" forState:UIControlStateNormal];
+    }else{
+        [imgView startAnimating];
+        [toggleButton setTitle:@"STOP" forState:UIControlStateNormal];
+    }
+    
+}
 
+@synthesize imgView;
+@synthesize toggleButton;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSArray *Images = [[NSArray alloc]initWithObjects:
+                       [UIImage imageNamed:@"1.jpg"],
+                       [UIImage imageNamed:@"2.jpg"],
+                       [UIImage imageNamed:@"3.jpg"],
+                       [UIImage imageNamed:@"4.jpg"],
+                       [UIImage imageNamed:@"5.jpg"],
+                       [UIImage imageNamed:@"6.jpg"],
+                       [UIImage imageNamed:@"7.jpg"],
+                       [UIImage imageNamed:@"8.jpg"],
+                       [UIImage imageNamed:@"9.jpg"],
+                       [UIImage imageNamed:@"10.jpg"],
+                       [UIImage imageNamed:@"11.jpg"],
+                       [UIImage imageNamed:@"12.jpg"],
+                       [UIImage imageNamed:@"13.jpg"],
+                       [UIImage imageNamed:@"14.jpg"],
+                       [UIImage imageNamed:@"15.jpg"],
+                       nil];
+    imgView.animationImages = Images;
+    imgView.animationDuration = 15.0;
+    
 }
 
 
